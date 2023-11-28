0f9a9a9228c927cac627c90bf26a4fc9dc02e786
dxkrnn <dxkrnn@gmail.com>
2023-11-28 22:40:15 +0700

    feat(ui):create signin signup

--- app/src/main/java/com/coolyeah/ecocleanx/ui/SigninActivity.kt
+++ app/src/main/java/com/coolyeah/ecocleanx/ui/SigninActivity.kt
@@ -4,23 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coolyeah.ecocleanx.R
-import com.coolyeah.ecocleanx.databinding.ActivityOnboardingEndBinding
+import com.coolyeah.ecocleanx.databinding.ActivitySigninBinding
 
-class OnboardingEndActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityOnboardingEndBinding
+class SigninActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOnboardingEndBinding.inflate(layoutInflater)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.btnMasuk.setOnClickListener{
-            startActivity(Intent(this, SigninActivity::class.java))
-            finish()
-        }
 
         binding.btnDaftar.setOnClickListener{
             startActivity(Intent(this, SignupActivity::class.java))
             finish()
         }
+
     }
 }
--- app/src/main/java/com/coolyeah/ecocleanx/ui/SignupActivity.kt
+++ app/src/main/java/com/coolyeah/ecocleanx/ui/SignupActivity.kt
@@ -4,22 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coolyeah.ecocleanx.R
-import com.coolyeah.ecocleanx.databinding.ActivityOnboardingEndBinding
+import com.coolyeah.ecocleanx.databinding.ActivitySignupBinding
 
-class OnboardingEndActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityOnboardingEndBinding
+class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOnboardingEndBinding.inflate(layoutInflater)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnMasuk.setOnClickListener{
             startActivity(Intent(this, SigninActivity::class.java))
-            finish()
-        }
-
-        binding.btnDaftar.setOnClickListener{
-            startActivity(Intent(this, SignupActivity::class.java))
             finish()
         }
     }
