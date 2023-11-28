8a203d3660ad12ff3132ab4eef0c3ac5940f6d8a
dxkrnn <dxkrnn@gmail.com>
2023-11-28 19:53:06 +0700

    create splash

---
+++ app/src/main/java/com/coolyeah/ecocleanx/ui/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.coolyeah.ecocleanx.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.coolyeah.ecocleanx.MainActivity
+import com.coolyeah.ecocleanx.R
+import com.coolyeah.ecocleanx.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.btnStart.setOnClickListener{
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
