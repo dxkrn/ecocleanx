bd5f25b4982db03529a330b54d75fa8bc3e447fd
dxkrnn <dxkrnn@gmail.com>
2023-12-09 13:24:52 +0700

    feat(int): create fun to get user location with gps

--- app/src/main/java/com/coolyeah/ecocleanx/ui/LaporInputActivity.kt
+++ app/src/main/java/com/coolyeah/ecocleanx/ui/LaporInputActivity.kt
@@ -1,18 +1,43 @@
 package com.coolyeah.ecocleanx.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.coolyeah.ecocleanx.R
+import android.widget.Toast
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.coolyeah.ecocleanx.databinding.ActivityLaporInputBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.Locale
 
 class LaporInputActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLaporInputBinding
+
+    //GPS
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private val permissionId = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLaporInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //GPS
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        //Buttons
         binding.btnBack.setOnClickListener{
             startActivity(Intent(this, BerandaActivity::class.java))
             finish()
@@ -21,6 +46,87 @@ class LaporInputActivity : AppCompatActivity() {
         binding.btnKirim.setOnClickListener {
             startActivity(Intent(this,LaporSuccessActivity::class.java))
             finish()
+        }
+
+        //get location on create
+        getLocation()
+    }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: MutableList<Address>? =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+//                        mainBinding.apply {
+//                            tvLatitude.text = "Latitude\n${list[0].latitude}"
+//                            tvLongitude.text = "Longitude\n${list[0].longitude}"
+//                            tvCountryName.text = "Country Name\n${list[0].countryName}"
+//                            tvLocality.text = "Locality\n${list[0].locality}"
+//                            tvAddress.text = "Address\n${list[0].getAddressLine(0)}"
+//                        }
+                        binding.textLatlong.text = "(Lat: " + list!![0].latitude + "\nLong: " + list!![0].longitude + ")"
+                        binding.textAddress.text = list!![0].getAddressLine(0)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == permissionId) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            }
         }
     }
 
---
+++ app/src/main/java/com/coolyeah/ecocleanx/ui/Test.java
@@ -0,0 +1,4 @@
+package com.coolyeah.ecocleanx.ui;
+
+public class Test {
+}
