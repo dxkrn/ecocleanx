a4f0e612995d3755b67aa98b04b0d8d74a9b0e09
nallruf <nalzaibaidin@gmail.com>
2023-12-10 11:20:17 +0700

    add nav

--- app/src/main/java/com/coolyeah/ecocleanx/ui/BerandaActivity.kt
+++ app/src/main/java/com/coolyeah/ecocleanx/ui/BerandaActivity.kt
@@ -3,8 +3,11 @@ package com.coolyeah.ecocleanx.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.coolyeah.ecocleanx.R
 import com.coolyeah.ecocleanx.databinding.ActivityBerandaBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BerandaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBerandaBinding
@@ -13,15 +16,20 @@ class BerandaActivity : AppCompatActivity() {
         binding = ActivityBerandaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnNotif.setOnClickListener{
-            startActivity(Intent(this,NotifikasiActivity::class.java))
-            finish()
-        }
+//        binding.btnNotif.setOnClickListener{
+//            startActivity(Intent(this,NotifikasiActivity::class.java))
+//            finish()
+//        }
+//
+//        binding.bannerLapor.setOnClickListener{
+//            startActivity(Intent(this,LaporInputActivity::class.java))
+//            finish()
+//        }
 
-        binding.bannerLapor.setOnClickListener{
-            startActivity(Intent(this,LaporInputActivity::class.java))
-            finish()
-        }
+        val bottomNavigationView =  findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController= findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
